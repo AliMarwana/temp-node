7c37d72a4ca292c218626abba3f58fe13122e321
unknown <alimarwana18@gmail.com>
2022-11-25 11:51:22 +0100

    temp node

---
+++ modules.js
@@ -0,0 +1,13 @@
+//Modules ``
+
+names = require('./4-variables')
+sayH = require('./5-utils')
+data = require('./6-alternative-flavor')
+require('./7-mind-grade')
+console.log(data)
+
+ sayH('Ali')
+ sayH('Marwana')
+ sayH('Abdou')
+ sayH(names.john)
+ sayH(names.peter)
